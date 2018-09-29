b6306113624f2260fa8c475ddb740139c0e36f34
Carlos Illobre <carlos.illobre@rocksaucestudios.com>
2018-09-29 15:05:32 -0300

    swagger setup

--- app/createExpressApp.js
+++ app/createExpressApp.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const expressWinston = require('express-winston')
 const apiRouter = require('./rest/createApiRouter.js')()
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./getSwaggerDocument.js')();
 
 module.exports = ({ database, logger }) => express()
 .use(expressWinston.logger({
@@ -11,6 +13,7 @@ module.exports = ({ database, logger }) => express()
 }))
 .use(bodyParser.urlencoded({ extended: true }))
 .use(bodyParser.json())
+.use('/rest/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, true))
 .use((req, res, next) => {
     req.base = `${req.protocol}://${req.get('host')}`
     req.logger = logger
@@ -18,7 +21,7 @@ module.exports = ({ database, logger }) => express()
     return next()
 })
 .use(express.static('./public'))
-.use('/api', apiRouter)
+.use('/rest', apiRouter)
 .use((error, req, res, next) => {
     logger.error(error, error)
     res.status(error.status || 500).json({ error })
